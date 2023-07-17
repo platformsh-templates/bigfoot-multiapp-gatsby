ab95af909f39b1fc59e5c6c408d0a3dbcc2228c2
Florent Huck <flovntp@gmail.com>
2023-07-17 10:07:47 +0200

    change app to api to respect api folder name

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ if ('PLATFORM_ROUTES' in process.env) {
   console.log('On a Platform.sh Environment');
   var data = decode(process.env.PLATFORM_ROUTES)
   const result = Object.entries(data)
-      .filter(([key, value]) => value.upstream == "app")
+      .filter(([key, value]) => value.upstream == "api")
       .map(([key, value]) => key)
   // index = 4;
   pathPrefix = '/site'
